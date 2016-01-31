ffdf4eac27e25da3d69f462aa5faf2f38f9b16ee
Bekt <bekt17@gmail.com>
2016-01-31 14:52:20 -0600

    Auto-commit: 2016-01-31

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -5,5 +5,10 @@ alias v vim
 alias g git
 alias gut git
 alias py3 python3
+alias dm docker-machine
+alias dc docker-compose
+
+# Open ports.
+alias lsp "sudo lsof -i -n -P | grep LISTEN"
 
 alias nvm "bass source ~/.nvm/nvm.sh ';' nvm"
--- fish/init.fish
+++ fish/init.fish
@@ -1,3 +1,12 @@
 echo "🙊  Fish init..."
 
 ln -s (brew --prefix nvm)/nvm.sh ~/.nvm/nvm.sh
+
+# Docker-machine.
+set -gx DOCKER_TLS_VERIFY "1"
+set -gx DOCKER_HOST "tcp://192.168.99.100:2376"
+set -gx DOCKER_CERT_PATH "~/.docker/machine/machines/default"
+set -gx DOCKER_MACHINE_NAME "default"
+
+set PATH $PATH ~/sdks/ngrok
+set PATH $PATH ~/sdks/terraform
