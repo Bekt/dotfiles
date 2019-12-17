2ce32b69efff105991ca9f4d77af733bc28588c2
Bekt <bekt17@gmail.com>
2019-12-17 16:00:03 -0600

    Auto-commit: 2019-12-17

--- fish/init.fish
+++ fish/init.fish
@@ -4,3 +4,7 @@ set -gx GREP_OPTIONS '--color=auto'
 set -gx GOPATH $HOME/workspace/golang
 set PATH "$GOPATH/bin" $PATH
 set PATH "/usr/local/Cellar/libpq/11.3/bin" $PATH
+
+# pyenv
+status --is-interactive
+source (pyenv init -|psub)
